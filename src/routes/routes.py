a93869a05377269edd9c10a028bd32a191f4beba
piavik <piavik@gmail.com>
2024-02-15 02:42:29 +0100

    routes

---
+++ src/routes/routes.py
@@ -0,0 +1,46 @@
+from typing import List
+
+from fastapi import APIRouter, HTTPException, Depends, status
+from sqlalchemy.orm import Session
+
+from src.database.db import get_db
+from src.schemas import contactModel, contactUpdate, contactStatusUpdate, contactResponse
+from src.workers import actions
+
+
+router = APIRouter(prefix='/contacts')
+
+
+@router.get("/", response_model=List[ContactResponse])
+async def read_contacts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    contacts = await actions.get_contacts(skip, limit, db)
+    return contacts
+
+
+@router.get("/{contact_id}", response_model=ContactResponse)
+async def read_contact(contact_id: int, db: Session = Depends(get_db)):
+    contact = await actions.get_contact(contact_id, db)
+    if contact is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="contact not found")
+    return contact
+
+
+@router.post("/", response_model=ContactResponse)
+async def create_contact(body: contactModel, db: Session = Depends(get_db)):
+    return await actions.create_contact(body, db)
+
+
+@router.put("/{contact_id}", response_model=ContactResponse)
+async def update_contact(body: contactUpdate, contact_id: int, db: Session = Depends(get_db)):
+    contact = await actions.update_contact(contact_id, body, db)
+    if contact is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="contact not found")
+    return contact
+
+
+@router.delete("/{contact_id}", response_model=ContactResponse)
+async def remove_contact(contact_id: int, db: Session = Depends(get_db)):
+    contact = await actions.remove_contact(contact_id, db)
+    if contact is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="contact not found")
+    return contact
